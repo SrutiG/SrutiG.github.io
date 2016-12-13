bf279bb2e8d7490f90a891593790999764d517f0
Sruti Guhathakurta <sruti@gatech.edu>
2016-12-12 21:53:29 -0500

    hacked solution for now

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -1,29 +1,20 @@
 $(document).ready(function() {
-    $(".projdescription").show();
+    var checkLoad = false;
     var projheight = $(".project").height();
     $(".projdescription").css("height", 2*projheight);
     $(".proj-text").css("height", 1.5*projheight);
     $(".proj-img").css("height", 1.5*projheight);
-    $(".project").map(function() {
-        var projindex = $(".project").index(this);
-        var images = $(".projdescription").eq(projindex).find(".proj-img");
-        var height = images.height();
-        var imageContainers = $(".projdescription").eq(projindex).find(".proj-images");
-        var imgHeight = imageContainers.height();
-        var diff = height-imgHeight;
-        if (diff > 20) {
-            images.css("padding-top", diff/2);
-            images.css("padding-bottom", diff/2);
-        }
-    });
     $(".projdescription").hide();
 
+    function loaded() {
+        checkLoad = true;
+        alert(checkLoad);
+    }
 
     $(".project").click(function() {
         var projindex = $(".project").index(this);
         $(".projdescription").eq(projindex).show();
         $(".project").hide();
-
     });
 
     $(".projdescription").click(function() {
